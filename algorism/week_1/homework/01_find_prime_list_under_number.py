8b67a28991a4d25119f7b0a3743d881460062c0c
WonJunPyun <84018317+WonJunPyun@users.noreply.github.com>
2021-06-12 14:15:42 +0900

    algorism 0612
    algorism is hell

--- algorism/week_1/homework/01_find_prime_list_under_number.py
+++ algorism/week_1/homework/01_find_prime_list_under_number.py
@@ -1,9 +1,53 @@
+# input = 20
+#
+#
+# def find_prime_list_under_number(number):
+#     prime_list = []
+#     for n in range(2, number + 1): # n 의 범위는 2부터 number 까지
+#         for i in range(2,n): # i 의 범위는 2 부터 n -1 까지
+#             if n % i == 0:
+#                 break
+#         else:
+#             prime_list.append(n)
+#
+#     return prime_list
+#
+#
+# result = find_prime_list_under_number(input)
+# print(result)
+
+# input = 20
+#
+#
+# def find_prime_list_under_number(number):
+#     prime_list = []
+#     for n in range(2, number + 1):
+#         for i in prime_list:
+#             if n % i == 0:
+#                 break
+#         else:
+#             prime_list.append(n)
+#
+#     return prime_list
+#
+#
+# result = find_prime_list_under_number(input)
+# print(result)
+
 input = 20
 
 
 def find_prime_list_under_number(number):
-    # 이 부분을 채워보세요!
-    return []
+    prime_list = []
+
+    for n in range(2, number + 1):
+        for i in prime_list:
+            if n % i == 0 and i * i <= n:
+                break
+        else:
+            prime_list.append(n)
+
+    return prime_list
 
 
 result = find_prime_list_under_number(input)
